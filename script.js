45faeb4a8fda2fda1736a2c65aae97644066b1f5
Thomas Steiner <tomac@google.com>
2024-09-30 12:48:34 +0200

    Add translate feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (async () => {
-  if (!('translation' in self)) {
+  if (!('translation' in self) && !(createDetector in self.translation)) {
     document.querySelector('.not-supported-message').hidden = false;
     return;
   }
@@ -11,18 +11,15 @@
   const language = document.querySelector('select');
 
   form.style.visibility = 'visible';
-  const translator = await self.translation.createDetector();
-
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    output.textContent = await translator.translate(
-      await translator.detect(input.value.trim())
-    );
-  });
+  const detector = await self.translation.createDetector();
 
   input.addEventListener('input', async () => {
+    if (!input.value.trim()) {
+      detected.textContent = 'not sure what language this is';
+      return;
+    }
     const { detectedLanguage, confidence } = (
-      await translator.detect(input.value.trim())
+      await detector.detect(input.value.trim())
     )[0];
     detected.textContent = `${(confidence * 100).toFixed(
       1
@@ -32,10 +29,37 @@
     )}`;
   });
 
+  input.dispatchEvent(new Event('input'));
+
   const languageTagToHumanReadable = (languageTag, targetLanguage) => {
     const displayNames = new Intl.DisplayNames([targetLanguage], {
       type: 'language',
     });
     return displayNames.of(languageTag);
   };
+
+  if ('createTranslator' in self.translation) {
+    document.querySelectorAll('[hidden]:not(.not-supported-message)').forEach((el) => {
+      el.removeAttribute('hidden');
+    });
+
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      try {
+        const sourceLanguage = (await detector.detect(input.value.trim()))[0].detectedLanguage;
+        if (!['en', 'ja'].includes(sourceLanguage)) {
+          output.textContent = 'Currently, only English ↔ Spanish and English ↔ Japanese are supported.';
+          return;
+        }
+        const translator = await self.translation.createTranslator({
+          sourceLanguage,
+          targetLanguage: language.value,
+        });
+        output.textContent = await translator.translate(input.value.trim());
+      } catch (err) {
+        output.textContent = 'An error occurred. Please try again.';
+        console.error(err.name, err.message);
+      }
+    });
+  }
 })();
